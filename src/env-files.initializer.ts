cccca8e25328e6849ca5a8e14d434e1b59727286
FreeIntelligence <free04381@gmail.com>
2022-02-28 06:18:41 -0300

    Se documentan las funciones.

--- src/env-files.initializer.ts
+++ src/env-files.initializer.ts
@@ -2,18 +2,32 @@ import { BaseInitializer } from '@dyna/core';
 import { config } from 'dotenv';
 import * as path from 'path';
 
+/**
+ * Load environment files (.env)
+ */
 export class EnvFilesInitializer extends BaseInitializer {
 
   public iIndex: number = Number.MIN_SAFE_INTEGER;
 
   async register() {
+    const dotfile = this.getEnvironmentFilePath();
+
+    if (!dotfile) {
+      return;
+    }
+
+    config({ path: dotfile });
+  }
+
+  getEnvironmentFilePath() {
     if (!this.app?.pkgPath) {
       return;
     }
 
-    config({
-      path: path.basename(this.app?.pkgPath),
-    });
+    const basename = path.basename(this.app.pkgPath);
+    const dotfile = path.join(basename, '.env');
+
+    return dotfile;
   }
 
 }
--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,9 @@
+/**
+ * Get environment value
+ * @param key variable key
+ * @param defaultValue if undefined
+ * @returns value
+ */
 export function env(key: string, defaultValue?: any) {
   return process.env[key] || defaultValue;
 }
